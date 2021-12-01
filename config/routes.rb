51f620b91a77f473166a0b71a2e34d7615709e34
Paul Anthony McGowan <xhostcom@gmail.com>
2021-12-01 01:59:30 +0000

    Ruby 2.7.3

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,5 @@ end
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
